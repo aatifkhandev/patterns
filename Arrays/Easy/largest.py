ada9761e59fb5d7bdb4f8105812e2339bd8b6dcf
Aatif <aatifkhan2001@gmail.com>
2025-01-14 15:06:55 +0530

    Modified

--- Arrays/Easy/largest.py
+++ Arrays/Easy/largest.py
@@ -1,13 +1,18 @@
+from typing import List
 #Finding largest element in an array
 
-def FindLargest(arr,n):
-    max=0
-    for i in range(0,n):
+def FindLargest(arr:List[int],n:int)->int:
+    max = arr[0]
+    for i in range(1,n):
         if max<arr[i]:
-            max=arr[i]
+            max = arr[i]
     return max
 
 
-arr=[1,2,3,4,5]
-n=5
-print(FindLargest(arr,n))
+def main():
+    arr = [1, 2, 3, 4, 5]
+    n = len(arr)  
+    print("The largest element in the array is:", FindLargest(arr, n))
+
+if __name__ == "__main__":
+    main()
