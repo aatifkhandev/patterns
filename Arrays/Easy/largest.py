19fd0cce26a0f27a7b911f33eed7b3f5267844ef
Aatif <aatifkhan2001@gmail.com>
2025-01-14 15:11:57 +0530

    Modified

--- Arrays/Easy/largest.py
+++ Arrays/Easy/largest.py
@@ -16,3 +16,39 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
+    
+# #include<bits/stdc++.h>
+
+# using namespace std;
+
+# int largest (int arr[], int n){
+     
+#      int max = INT_MIN;
+
+#      for(int i=0;i<n;i++){
+#         if(arr[i]>max){
+#             max=arr[i];
+#         }
+#      }
+#      return max;
+
+# }
+
+
+
+# int main(){
+# int n;
+# cout<<"enter the number of elements";
+# cin>>n;
+# int arr[n];
+
+# for(int i=0;i<n;i++){
+# cin>>arr[i];
+# }
+# largest(arr,n);
+# for(int i=0;i<n;i++){
+#     cout<<arr[i]<<endl;
+# }
+
+# }
