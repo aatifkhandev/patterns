d04f7b8d1955b206ec1f5d8889fa47ddf9326b27
Aatif <aatifkhan2001@gmail.com>
2025-01-29 15:00:31 +0530

    29-Jan-2025

--- Arrays/Easy/moveZeros.py
+++ Arrays/Easy/moveZeros.py
@@ -4,4 +4,27 @@ from typing import List
 
 
 def moveZero(arr:List[int],n:int)->int:
-    return
+    temp = []
+    
+    for i in range(n):
+        if arr[i]!=0:
+            temp.append(arr[i])
+            
+    nz = len(temp)
+    
+    
+    for i in range(nz):
+        arr[i] = temp[i]
+        
+    
+    for i in range(nz ,n):
+        arr[i] = 0
+        
+    
+    return arr
+
+
+
+arr = [1,0,2,0,3,0,4,5]
+n = len(arr)
+print(moveZero(arr,n))
