920fdd95e87c19ee9f2b6c01b4bf40588f1c73f7
Aatif <aatifkhan2001@gmail.com>
2025-01-29 22:57:03 +0530

    Suzie I Miss you  29-Jan-2025

--- Arrays/Easy/moveZeros.py
+++ Arrays/Easy/moveZeros.py
@@ -24,6 +24,36 @@ def moveZero(arr:List[int],n:int)->int:
     return arr
 
 
+def moveZerosOptimized(arr:List[int],  n: int) -> int:
+    j = -1
+    # Place the pointer j
+    for i in range(n):
+        if arr[i] == 0:
+            j = i
+            break
+    
+    # No non-zero elements
+    if j == -1:
+        return arr
+    
+    # Move the pointers i and j and swap accordingly
+    for i in range(j + 1, n):
+        if arr[i] != 0:
+            arr[i], arr[j] = arr[j], arr[i]
+            j += 1
+    
+    return arr
+
+
+arr = [1, 0, 2, 3, 2, 0, 0, 4, 5, 1]
+n = 10
+ans = moveZerosOptimized(arr, n)
+for it in ans:
+    print(it, end=' ')
+print()
+
+
+
 
 arr = [1,0,2,0,3,0,4,5]
 n = len(arr)
