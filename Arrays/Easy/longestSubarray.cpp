8e5649ba6dedb566c77fe50fa4e834224fbc5038
Aatif <aatifkhan2001@gmail.com>
2025-01-30 09:17:53 +0530

    30-Jan-2025

---
+++ Arrays/Easy/longestSubarray.cpp
@@ -0,0 +1,8 @@
+from typing import List
+// Input : N = 3, k = 5, array[] = {2,3,5}
+// Result: 2
+// The longest subarray with sum 5 is {2, 3}. And its length is 2.
+
+// Input : N = 5, k = 10, array[] = {2,3,5,1,9}
+// Result: 3
+//  The longest subarray with sum 10 is {2, 3, 5}. And its length is 3.
