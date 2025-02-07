32030b8b146a8a46bd11ed9fc0d43559de04344d
Aatif <aatifkhan2001@gmail.com>
2025-02-07 10:46:15 +0530

    Added removeDuplicates optimized

--- Arrays/Easy/removeDup.cpp
+++ Arrays/Easy/removeDup.cpp
@@ -19,12 +19,22 @@ int removeDup(int arr[],int n){
     return k;
 }
 
+int removeDupOptimized(int arr[] , int n){
+    int i=0;
+    for(int j=1;j<n;j++){
+        if(arr[i]!=arr[j]){
+            i++;
+            arr[i]=arr[j];
+        }
+    }
+    return i+1;
+}
 
 
 int main(){
     int arr[] = {1,1,1,2,2,2,3,3,3};
    int n = sizeof(arr)/sizeof(arr[0]);
-    int ans = removeDup(arr,n);
+    int ans = removeDupOptimized(arr,n);
 
     for(int i=0;i<ans;i++){
         cout<<arr[i]<<endl;
