102395a8232e80f9e062449202c3c95f3168c5eb
Aatif <aatifkhan2001@gmail.com>
2025-02-26 09:07:04 +0530

    Modified binary search

--- Searching/BinarySearch/binarySearch.py
+++ Searching/BinarySearch/binarySearch.py
@@ -1,32 +1,21 @@
-# Binary search
+# # Binary search
 
 def binarySearch(arr,n,target):
     low = 0
     high = n-1
-    mid = (low+high)//2
-    while(low<=high):
-        
-        if(arr[mid]==target):
-            
-            return target-1
-        
-        elif(arr[mid]<target):
+    while low<=high:
+        mid = (low + high) // 2 
+        if arr[mid]==target:
+            return mid
+        elif arr[mid]<target:
             low = mid+1
-        elif(arr[mid]>target):
-    
-    
+        elif arr[mid]>target:
             high = mid-1
-    return mid
-    
-    
-
-
-
-
+    return -1
 
 
 
 arr = [3, 4, 6, 7, 9, 12, 16, 17]
-target = 6
+target = 17
 n = len(arr)
 print(binarySearch(arr,n,target))
