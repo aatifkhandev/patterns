4915cd1fb49e53288e9915cb4b8d1df5a114e800
Aatif <aatifkhan2001@gmail.com>
2025-04-18 23:38:05 +0530

    added recursion

--- Recursion/practice.py
+++ Recursion/practice.py
@@ -1,11 +1,24 @@
 
+# count = 0
+# def Aatif(count):
+#     if count>=10:
+#         return
+#     count+=1
+    
+#     print(count)
+#     Aatif(count)
+    
+# Aatif(count)
+
 count = 0
-def Aatif(count):
+
+def f(count):
     if count>=10:
         return
+    print(count)
     count+=1
     
-    print(count)
-    Aatif(count)
+    f(count)
     
-Aatif(count)
+
+f(count)
