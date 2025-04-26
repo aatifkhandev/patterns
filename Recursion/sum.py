f28f382f3ef21baceafd0376f64679ec8d20cb3c
Aatif <aatifkhan2001@gmail.com>
2025-04-26 14:11:18 +0530

    Added Functional version

--- Recursion/sum.py
+++ Recursion/sum.py
@@ -8,7 +8,7 @@
 # print(my_sum(1,5))
 
 
-def sumRecursive(i,sum):
+def sumRecursive(i,sum): # parameter
     if i<1:
         print(sum)
         return
@@ -16,8 +16,17 @@ def sumRecursive(i,sum):
     sumRecursive(i-1,sum+i)
     
     
-n = int(input("Enter a number"))
-sumRecursive(n,0)
+# n = int(input("Enter a number"))
+# sumRecursive(n,0)
         
+
+def FunctionalSum(n) :  # Functional recursion 
+    if n==0:
+        return 0
+    
+    return n+FunctionalSum(n-1)
+
+
+print(FunctionalSum(5))
     
     
