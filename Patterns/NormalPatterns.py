06ba58798c4f15975177087e312bdf9605741a70
Aatif <aatifkhan2001@gmail.com>
2025-04-30 15:39:32 +0530

    Added patterns

--- Patterns/NormalPatterns.py
+++ Patterns/NormalPatterns.py
@@ -126,4 +126,28 @@ def star(n):
         print()
 
 
-star(5)
+# star(5)
+
+
+# Pattern - 8: Inverted Star Pyramid
+
+# ***********
+#  *********
+#   *******
+#    ***** 
+#     ***    
+#      *
+
+def InvertedStar(n):
+    for i in range(n):
+        for j in range(i):
+            print(" ", end='')
+        for j in range(2*n-(2*i+1)):
+            print("*",end='')
+        for j in range(i):
+            print(" ",end='')
+        print()
+
+
+
+InvertedStar(5)
