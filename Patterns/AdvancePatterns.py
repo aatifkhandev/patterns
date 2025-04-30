b21b171d5b9fed59a79f047ad03c92a77b7a139f
Aatif <aatifkhan2001@gmail.com>
2025-04-30 16:12:40 +0530

    Added Patterns

--- Patterns/AdvancePatterns.py
+++ Patterns/AdvancePatterns.py
@@ -19,4 +19,27 @@ def BinaryPattern(n):
         
 
 
-BinaryPattern(5)
+# BinaryPattern(5)
+
+
+# Pattern - 13: Increasing Number Triangle Pattern
+
+# 1
+# 2  3
+# 4  5  6
+# 7  8  9  10
+# 11  12  13  14  15
+# 16  17  18  19  20  21
+
+
+def triangle(n):
+    num = 1
+    for i in range(n):
+        for j in range(i):
+            
+            print(num,end=' ')
+            num = num+1
+        print()
+        
+# triangle(5)
+
