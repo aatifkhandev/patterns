ce9006caf6e5c3247c5a9f48a4bad4eb89cf55ff
Aatif <aatifkhan2001@gmail.com>
2025-05-20 20:14:11 +0530

    Added fibonacci

---
+++ Recursion/fibonacci.py
@@ -0,0 +1,14 @@
+# Naive Approach -> using for loop
+def FibUsingLoop(n):
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    fib = [0,1]
+    for i in range(2,n+1):
+        fib.append(fib[i-1]+fib[i-2])
+    return fib[n]
+        
+        
+print(FibUsingLoop(4))
+        
