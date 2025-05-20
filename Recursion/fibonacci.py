6ddf5696ed8dbaf5401583fdf099f82220cbf7a8
Aatif <aatifkhan2001@gmail.com>
2025-05-20 20:27:33 +0530

    Added Fibonacci number

--- Recursion/fibonacci.py
+++ Recursion/fibonacci.py
@@ -1,3 +1,31 @@
+# Optimized code
+
+def fib(n):
+    if n<=1:
+        return n
+    last = fib(n-1)
+    secondLast = fib(n-2)
+    return last+secondLast
+
+print(fib(4))
+
+# Better code - whole series
+
+def fibonacci_series(n):
+    if n == 0:
+        return [0]
+    elif n == 1:
+        return [0, 1]
+    
+    series = [0, 1]
+    for i in range(2, n):
+        series.append(series[-1] + series[-2])
+    
+    return series
+
+
+# print(fibonacci_series(5))
+
 # Naive Approach -> using for loop
 def FibUsingLoop(n):
     if n==0:
@@ -10,5 +38,5 @@ def FibUsingLoop(n):
     return fib[n]
         
         
-print(FibUsingLoop(4))
+# print(FibUsingLoop(4))
         
