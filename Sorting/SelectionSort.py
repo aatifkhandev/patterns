71de9ef91f707cabdf9e1da032529b9381dbf024
Aatif <aatifkhan2001@gmail.com>
2025-05-21 17:16:06 +0530

    Fix : Modified Selection Sort

--- Sorting/SelectionSort.py
+++ Sorting/SelectionSort.py
@@ -2,7 +2,7 @@ def SelectionSort(arr):
     n = len(arr)
     for i in range(n-1):
         minIndex = i
-        for j in range(i,n):
+        for j in range(i+1,n):
             if arr[j]<arr[minIndex]:
                 minIndex = j
         arr[i],arr[minIndex] = arr[minIndex],arr[i]
