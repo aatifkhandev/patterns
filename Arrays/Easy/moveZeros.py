a63201558ca9ef0363750346a199070316cc6ea1
Aatif <aatifkhan2001@gmail.com>
2025-06-26 19:58:36 +0530

    Feat : Added MoveZero Naive

--- Arrays/Easy/moveZeros.py
+++ Arrays/Easy/moveZeros.py
@@ -1,30 +1,27 @@
-from typing import List
-# i/p : [1,0,2,0,3,0,4,0]
-#o/p : [1,2,3,4,5,0,0,0,0,0]
-
-
-def moveZero(arr:List[int],n:int)->int:
-    temp = []
+def moveZero(arr):
+    n = len(arr)
+    temp = [0]*n
+    
+    j = 0
     
     for i in range(n):
         if arr[i]!=0:
-            temp.append(arr[i])
+            temp[j] = arr[i]
+            j+=1
             
-    nz = len(temp)
-    
-    
-    for i in range(nz):
+    while j<n:
+        temp[j] = 0
+        j+=1
+        
+    for i in range(n):
         arr[i] = temp[i]
-        
-    
-    for i in range(nz ,n):
-        arr[i] = 0
-        
     
     return arr
+     
+    
 
 
-def moveZerosOptimized(arr:List[int],  n: int) -> int:
+# def moveZerosOptimized(arr:List[int],  n: int) -> int:
     j = -1
     # Place the pointer j
     for i in range(n):
@@ -46,15 +43,17 @@ def moveZerosOptimized(arr:List[int],  n: int) -> int:
 
 
 arr = [1, 0, 2, 3, 2, 0, 0, 4, 5, 1]
-n = 10
-ans = moveZerosOptimized(arr, n)
-for it in ans:
-    print(it, end=' ')
-print()
+moveZero(arr)
+print(arr)
+# n = 10
+# ans = moveZerosOptimized(arr, n)
+# for it in ans:
+#     print(it, end=' ')
+# print()
 
 
 
 
-arr = [1,0,2,0,3,0,4,5]
-n = len(arr)
-print(moveZero(arr,n))
+# arr = [1,0,2,0,3,0,4,5]
+# n = len(arr)
+# print(moveZero(arr,n))
