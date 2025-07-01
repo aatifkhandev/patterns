96911b43db51ac3c7682be70d5cebb35e5756262
Aatif <aatifkhan2001@gmail.com>
2025-07-01 23:38:23 +0530

    Feat : Added Missing Number

---
+++ Arrays/Easy/missingNumber.py
@@ -0,0 +1,15 @@
+def missingNumber(arr):
+    n = len(arr)
+    expected_sum = (n*(n+1)//2)
+    actual_sum = 0
+    for i in range(n):
+        actual_sum+=arr[i]
+        
+    return expected_sum-actual_sum
+        
+
+
+
+
+arr = [3,0,1]
+print(missingNumber(arr))
