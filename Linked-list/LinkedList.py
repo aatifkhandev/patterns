641002f2fe9242bc06a9cf1de7e22970b89ca299
Aatif <aatifkhan2001@gmail.com>
2025-07-27 15:43:53 +0530

    Feat : Added List Class

--- Linked-list/LinkedList.py
+++ Linked-list/LinkedList.py
@@ -7,3 +7,12 @@ class List:
     def __init__(self):
         self.head = None
         self.tail = None
+    
+    def push_front(self,val):
+        node = Node(val)
+        if self.head==None:
+            self.head=self.tail=Node
+        else:
+            node.next = self.head
+            self.head = node
+            
