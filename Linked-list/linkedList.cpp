266080efe7fc7bf536e7d8a589499912a468a521
Aatif <aatifkhan2001@gmail.com>
2025-07-27 14:31:18 +0530

    Feat : Added Node Class

---
+++ Linked-list/linkedList.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;
+
+    Node(int val){
+        data = val;
+        next = NULL
+    }
+}
+
+
+int main(){
+    Node *newNode = new Node(5);
+    print(Node)
+}
